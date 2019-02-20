8f2196a8df04e0be23675c322af3a99842686dfa
edwardho <ekinbenho@gmail.com>
2019-02-19 23:42:57 -0500

    Completed TLIB.03-Exercise

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -58,11 +58,25 @@ class Emojifier {
             Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
         }
 
-        // TODO (2): Iterate through the faces, calling getClassifications() for each face.
+        // COMPLETE (2): Iterate through the faces, calling getClassifications() for each face.
+        for (int i = 0; i < faces.size(); ++i) {
+            Face face = faces.valueAt(i);
+            // Log the classifiation probabilities for each face
+            getClassifications(face);
+        }
 
         // Release the detector
         detector.release();
     }
 
-    // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    // COMPLETE (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    static void getClassifications(Face face) {
+        // Log all probabilities
+        Log.d(LOG_TAG, "getClassiciations: smilingProbability = " +
+                face.getIsSmilingProbability());
+        Log.d(LOG_TAG, "getClassiciations: leftEyeOpenProbability = " +
+                face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG, "getClassiciations: rightEyeOpenProbability = " +
+                face.getIsRightEyeOpenProbability());
+    }
 }
